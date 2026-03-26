bdd28ebeaf9679f61e342928acb83f81461310d0
Kabil Dev <kabildev@datanetiix.com>
2026-03-26 21:27:18 +0530

    Test bookings

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -12,99 +12,123 @@ export const fetchMovieById = async (id) => {
   return res.data;
 };
 
-export const registerUser = async (newUser) =>{
+export const registerUser = async (newUser) => {
   const res = await axios.post(`${BASE_URL}/register`, newUser); // newUser => post Data
   return res.data;
-}
+};
 
 export const loginUser = async (userCreds) => {
   const res = await axios.post(`${BASE_URL}/login`, userCreds); // newUser => post Data
   return res.data;
-}
+};
 
 export const fetchProfile = async () => {
   const res = await axios.get(`${BASE_URL}/profile`, {
     headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`
-    }
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   });
   return res.data;
-}
+};
 
 export const createTheatre = async (newTheatre) => {
-    const res = await axios.post(`${BASE_URL}/theatres`, newTheatre, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
-    });
-    return res.data;
-}
+  const res = await axios.post(`${BASE_URL}/theatres`, newTheatre, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  return res.data;
+};
 
 export const fetchTheatres = async () => {
-    const res = await axios.get(`${BASE_URL}/theatres`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`
-      }
-    });
-    return res.data;
-}
+  const res = await axios.get(`${BASE_URL}/theatres`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  return res.data;
+};
 
 export const fetchTheatreById = async (id) => {
-    const res = await axios.get(`${BASE_URL}/theatres/${id}`, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
-    });
-    return res.data;
-}
+  const res = await axios.get(`${BASE_URL}/theatres/${id}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  return res.data;
+};
 
-export const createScreening = async ({ theatreId, movieId, price, showTimings }) => {
-    const res = await axios.post(
-        `${BASE_URL}/screenings`,
-        { theatreId, movieId, price, showTimings },
-        {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        }
-    );
-    console.log("Cre_Book_res: ", res);
-    return res.data;
-}
+export const createScreening = async ({
+  theatreId,
+  movieId,
+  price,
+  showTimings,
+}) => {
+  const res = await axios.post(
+    `${BASE_URL}/screenings`,
+    { theatreId, movieId, price, showTimings },
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    },
+  );
+  console.log("Cre_Book_res: ", res);
+  return res.data;
+};
 
 export const fetchScreeningsByMovieId = async (movieId) => {
-    const res = await axios.get(`${BASE_URL}/screenings/${movieId}`);
-    return res.data;
-}
+  const res = await axios.get(`${BASE_URL}/screenings/${movieId}`);
+  return res.data;
+};
 
-export const createBooking = async ({ theatreId, movieId, seats, showTime, amount }) => {
-    const res = await axios.post(
-        `${BASE_URL}/bookings`,
-        { theatreId, movieId, seats, showTime, amount },
-        {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        }
-    );
-    return res.data;
-}
+export const createBooking = async ({
+  theatreId,
+  movieId,
+  seats,
+  showTime,
+  amount,
+}) => {
+  const res = await axios.post(
+    `${BASE_URL}/bookings`,
+    { theatreId, movieId, seats, showTime, amount },
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    },
+  );
+  return res.data;
+};
 
 export const fetchMyBookings = async () => {
+  try {
+    console.log("BASE_URL:", BASE_URL);
+
+    const token = localStorage.getItem("token");
+    console.log("TOKEN:", token);
     const res = await axios.get(`${BASE_URL}/bookings`, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
+    console.log("Bookings:", res.data);
     return res.data;
-}
+  } catch (err) {
+    console.log("Fetch bookings error:", err.response?.data || err.message);
+    throw err;
+  }
+};
 
 export const forgotPassword = async ({ email }) => {
-    const res = await axios.post(`${BASE_URL}/forgot-password`, { email });
-    return res.data;
-}
+  const res = await axios.post(`${BASE_URL}/forgot-password`, { email });
+  return res.data;
+};
 
 export const resetPassword = async ({ token, password }) => {
-    const res = await axios.post(`${BASE_URL}/reset-password`, { token, password });
-    return res.data;
-}
+  const res = await axios.post(`${BASE_URL}/reset-password`, {
+    token,
+    password,
+  });
+  return res.data;
+};
